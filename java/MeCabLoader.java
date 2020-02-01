d5ff8543ad522d57eeade4d97669750419b4f7d8
Seunghwan Hong <hj332921@gmail.com>
2020-02-02 03:56:24 +0900

    feat: OS 이름 받아서 사용하도록 추가

--- java/MeCabLoader.java
+++ java/MeCabLoader.java
@@ -7,7 +7,7 @@ public class MeCabLoader {
         System.out.println("Loading MeCab library...");
         String targetFolder = "/lib";
         String libraryFileName = "libMeCab";
-        if (System.getProperty("os.name").indexOf("Mac") >= 0) {
+        if (osName.indexOf("Mac") >= 0) {
             System.out.println("Environment macOS detected.");
             libraryFileName += ".dylib";
         } else {
@@ -16,15 +16,13 @@ public class MeCabLoader {
         }
         
         File libraryFile = new File(targetFolder, libraryFileName);
-        if (libraryFile.exists()) {
-            try {
-                System.load(libraryFile.getAbsolutePath());
-                System.out.println("MeCab was successfully loaded.");
-            } catch (Exception e) {
-                System.out.println("Exception was occurred while loading library file");
-                e.printStackTrace();
-                System.exit(1);
-            }
+        try {
+            System.load(libraryFile.getAbsolutePath());
+            System.out.println("MeCab was successfully loaded.");
+        } catch (Exception e) {
+            System.out.println("Exception was occurred while loading library file");
+            e.printStackTrace();
+            System.exit(1);
         }
     }
 }
