6987fbfc2efd890a722b492fc0121d8cd1b613b8
Seunghwan Hong <hj332921@gmail.com>
2020-02-01 19:21:15 +0900

    fix: 빌드 결과 library 파일을 올바른 위치에 생성하도록 수정

--- java/run-linux.sh
+++ java/run-linux.sh
@@ -4,9 +4,8 @@ INCLUDE=$JAVA_HOME/include
 PACKAGE=org/chasen/mecab
 
 build_lib_linux() {
-    mkdir -p lib/linux
     g++ -O1 -c -fpic MeCab_wrap.cxx  $(mecab-config --cflags) -I$INCLUDE -I$INCLUDE/linux
-    g++ -shared  MeCab_wrap.o -o lib/linux/libMeCab.so $(mecab-config --libs)
+    g++ -shared  MeCab_wrap.o -o libMeCab.so $(mecab-config --libs)
 }
 
 build_lib_linux
--- java/run-macos.sh
+++ java/run-macos.sh
@@ -4,9 +4,8 @@ INCLUDE=$JAVA_HOME/include
 PACKAGE=org/chasen/mecab
 
 build_lib_macos() {
-    mkdir -p lib/macos
     g++ -O1 -c -fpic MeCab_wrap.cxx  $(mecab-config --cflags) -I$INCLUDE -I$INCLUDE/darwin
-    g++ -dynamiclib  MeCab_wrap.o -o lib/macos/libMeCab.dylib $(mecab-config --libs)
+    g++ -dynamiclib  MeCab_wrap.o -o libMeCab.dylib $(mecab-config --libs)
 }
 
 build_lib_macos
