34c0b6c00596efc51d2b34b9e376e1d4613b09e7
Seunghwan Hong <hj332921@gmail.com>
2020-02-02 15:56:31 +0900

    feat: MeCab 클래스에 로딩 함수 추가

--- java/run-build.sh
+++ java/run-build.sh
@@ -3,7 +3,7 @@
 PACKAGE=org/chasen/mecab
 
 build_jar() {
-    cp MeCabLoader.java org/chasen/mecab/MeCabLoader.java
+    cp -f MeCab.java org/chasen/mecab/MeCab.java
     javac $PACKAGE/*.java
     jar cfv MeCab.jar $PACKAGE/*.class lib/libMeCab.dylib lib/libMeCab.so
 }
