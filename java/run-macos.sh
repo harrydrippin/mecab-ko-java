ba2b8ffeb75837654141c02912a2a8851eb3d5fe
Seunghwan Hong <hj332921@gmail.com>
2020-02-03 21:27:29 +0900

    feat: -rpath 옵션 추가
    Signed-off-by: Seunghwan Hong <hj332921@gmail.com>

--- java/run-macos.sh
+++ java/run-macos.sh
@@ -6,7 +6,6 @@ PACKAGE=org/chasen/mecab
 build_lib_macos() {
     mkdir -p $PACKAGE/native/macos
     g++ -O1 -c -fpic MeCab_wrap.cxx  $(mecab-config --cflags) -I$INCLUDE -I$INCLUDE/darwin
-    g++ -dynamiclib  MeCab_wrap.o -o $PACKAGE/native/macos/libMeCab.dylib $(mecab-config --libs)
-}
+    g++ -shared MeCab_wrap.o -o $PACKAGE/native/macos/libMeCab.so -Wl,-rpath,./ $(mecab-config --libs)}
 
 build_lib_macos
